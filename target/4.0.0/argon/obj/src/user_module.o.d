93062204075aefdac19481d5d82c8630e2aca601
mrl1621 <113121638+mrl1621@users.noreply.github.com>
2022-09-15 12:02:34 -0400

    Add files via upload

---
+++ target/4.0.0/argon/obj/src/user_module.o.d
@@ -0,0 +1,6 @@
+c\:/Users/scorp/OneDrive/Desktop/OCN479/SOS/target/4.0.0/argon//obj/./src/user_module.o: \
+ src/user_module.c \
+ ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
+ ../../../dynalib/inc/dynalib.h
+../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
+../../../dynalib/inc/dynalib.h:
